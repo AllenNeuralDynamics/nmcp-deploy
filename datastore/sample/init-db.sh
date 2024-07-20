aae207887e11b9733ec6e5b9dc15ca7a86e89d1f
Patrick Edson <pedson@users.noreply.github.com>
2024-07-20 09:38:40 -0400

    NMCP naming.

--- datastore/sample/init-db.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE DATABASE samples_production;
-    GRANT ALL PRIVILEGES ON DATABASE samples_production TO "$POSTGRES_USER";
-EOSQL
