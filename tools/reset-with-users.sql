4d5b38d34c2c0ca2802c26d992f584d40d57e6c6
Patrick Edson <pedson@users.noreply.github.com>
2025-10-11 18:41:45 -0400

    Version update.

---
+++ tools/reset-with-users.sql
@@ -0,0 +1,16 @@
+--
+-- Clears all data except users.
+--
+DELETE FROM "Issue";
+DELETE FROM "SearchContent";
+DELETE FROM "Precomputed";
+DELETE FROM "TracingNode";
+DELETE FROM "Tracing";
+DELETE FROM "Reconstruction";
+DELETE FROM "Neuron";
+DELETE FROM "Sample";
+DELETE FROM "Genotype";
+DELETE FROM "Injection";
+DELETE FROM "InjectionVirus";
+DELETE FROM "Fluorophore";
+DELETE FROM "SynchronizationMarker";
