c92cccf6799bf9d6781334242c5177e0663278d8
Patrick Edson <pedson@users.noreply.github.com>
2025-10-12 17:10:50 -0400

    Version updates.

--- tools/reset-with-users.sql
+++ tools/reset-with-users.sql
@@ -7,10 +7,11 @@ DELETE FROM "Precomputed";
 DELETE FROM "TracingNode";
 DELETE FROM "Tracing";
 DELETE FROM "Reconstruction";
-DELETE FROM "Neuron";
-DELETE FROM "Sample";
-DELETE FROM "Genotype";
 DELETE FROM "Injection";
 DELETE FROM "InjectionVirus";
 DELETE FROM "Fluorophore";
+DELETE FROM "Neuron";
+DELETE FROM "Sample";
+DELETE FROM "Genotype";
 DELETE FROM "SynchronizationMarker";
+
