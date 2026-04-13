965a85f5361fbc7c590aa2cf59ff4b81624ca86d
Patrick Edson <pedson@users.noreply.github.com>
2026-04-13 15:06:44 -0400

    Simplify resetting precomputed storage.

---
+++ reset_precomputed.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+. ./.env
+
+set -e
+
+top_level=("axon" "dendrite" "full" "specimen")
+
+recursive=("segment_properties" "skeleton")
+
+non_recursive=("info")
+
+for top in "${top_level[@]}"; do
+    for sub in "${recursive[@]}"; do
+        location="${NMCP_PRECOMPUTED_OUTPUT}/${top}/${sub}"
+        echo "clearing location ${location} (recursive)"
+        aws s3 rm $location --recursive
+    done
+    for sub in "${non_recursive[@]}"; do
+        location="${NMCP_PRECOMPUTED_OUTPUT}/${top}/${sub}"
+        echo "clearing location ${location} (non-recursive)"
+        aws s3 rm $location
+    done
+done
